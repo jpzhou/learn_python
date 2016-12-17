c25c06e85aa29ae1c0708857d648272c4e02b161
jpzhou <jiping_zhou@intsig.net>
2016-12-17 17:51:50 +0800

    pyspark  udf

---
+++ learn_pyspark_udf.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------------
+# Name:
+# Author:      jp $
+# -------------------------------------------------------------------------------
+from pyspark.sql.types import BooleanType, Row
+from pyspark import SQLContext, SparkContext
+sc = SparkContext()
+sqlContext = SQLContext()
+
+
+def check_only_contain_chinese(check_str):
+    for ch in check_str.strip():
+        if not u'\u4e00' <= ch <= u'\u9fa5':
+            return False
+    return True
+
+sqlContext.registerFunction('check_only_contain_chinese', check_only_contain_chinese, BooleanType())
+
+# load text data
+line = sc.textFile("/tmp/test_person_score.txt")
+parts = line.map(lambda l: l.split())
+people = parts.map(lambda p: Row(name=p[0], phone=p[1]))
+schemaPeople = sqlContext.createDataFrame(people)
+
+# write text data
+schemaPeople.write.text('/tmp/test_person_score_txt')
+
